37033dfd939980725b41ba28c2ffe19c2dd28fc9
87982 <87982@YUHANG>
2023-01-31 17:41:16 -0500

    Add project files.

---
+++ Deliverable (3) - Demo/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Deliverable__3____Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+            string series = "";
+
+            try
+            {
+                while (true)
+                {
+                    Console.Write("Enter an integer number between 1 and 100: ");
+                    input = int.Parse(Console.ReadLine());
+
+                    if (input >= 1 && input <= 100)
+                    {
+                        break;
+                    }
+                }
+
+                while (true)
+                {
+                    Console.Write("Specify the series tye: Even or Odd ");
+                    series = Console.ReadLine();
+
+                    if (series == "Even" || series == "Odd")
+                    {
+                        break;
+                    }
+                }
+
+                if (series == "Odd")
+                {
+                    Console.Write("You have selected " + series + " series. The numbers between 0 and " + input + " are:");
+                    for (int i = 1; i <= input; i = i + 2)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+                if (series == "Even")
+                {
+                    Console.Write("You have selected " + series + " series. The numbers between 0 and " + input + " are:");
+                    for (int i = 0; i <= input; i = i + 2)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Enter a numeric value");
+            }
+        }
+    }
+}
